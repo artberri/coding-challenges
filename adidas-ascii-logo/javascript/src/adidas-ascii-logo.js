d65de44338b57e00e6818cea84807144109e6401
Alberto Varela <avarela@plainconcepts.com>
2018-11-24 08:29:37 +0100

    Solution by ALberto Varela

--- adidas-ascii-logo/javascript/src/adidas-ascii-logo.js
+++ adidas-ascii-logo/javascript/src/adidas-ascii-logo.js
@@ -4,5 +4,21 @@
  * @returns {string} adidas logo.
  */
 module.exports = function(width) {
-  return `${ width }`;
+  if (width < 2) {
+    throw new Error('Error, minimun width is 2');
+  }
+
+  const output = [];
+  const smallestStripeHeight = Math.round(Math.sqrt(width));
+  const repeatCharByWidth = (char) => char.repeat(width);
+  const piece = (rowNumber, columnNumber) =>
+    rowNumber / smallestStripeHeight > 3 - columnNumber
+      ? repeatCharByWidth('@') + ' '.repeat(smallestStripeHeight)
+      : repeatCharByWidth(' ');
+
+  for (let i = 0; i < 3 * smallestStripeHeight; i++) {
+    output.push(`${ ' '.repeat(i % smallestStripeHeight) }${ piece(i + 1, 1) }${ piece(i + 1, 2) }${ repeatCharByWidth('@') }`);
+  }
+
+  return output.join('\n');
 };
